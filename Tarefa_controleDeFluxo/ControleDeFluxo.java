143169bdb88e1d1fa91fc6c0b020614b32b63e61
Gabriel Just <gabrieljust123@gmail.com>
2024-02-22 15:57:16 -0300

    commit controle de fluxo 2

--- Tarefa_controleDeFluxo/ControleDeFluxo.java
+++ Tarefa_controleDeFluxo/ControleDeFluxo.java
@@ -1,2 +1,23 @@
-package PACKAGE_NAME;public class ControleDeFluxo {
+public class ControleDeFluxo {
+    public static void main(String[] args) {
+        int nota1 = 7;
+        int nota2 = 9;
+        int nota3 = 6;
+        int nota4 = 8;
+
+        int mediaNotas = (nota1 + nota2 + nota3 + nota4)/4;
+        System.out.println(mediaNotas);
+        if (mediaNotas >=7) {
+            System.out.println("Aluno foi aprovado!");
+        }
+        else if (mediaNotas >=5) {
+            System.out.println("Aluno esta em recuperação!");
+        }
+        else {
+            System.out.println("Aluno foi reprovado!");
+        }
+    }
 }
+
+
+
