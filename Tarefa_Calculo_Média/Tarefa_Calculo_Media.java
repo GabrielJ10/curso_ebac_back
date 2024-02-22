910c13f539c00d63571895b9ff7bb613596ffea6
Gabriel Just <gabrieljust123@gmail.com>
2024-02-22 15:35:56 -0300

    commit tarefa calculo de media

---
+++ Tarefa_Calculo_Média/Tarefa_Calculo_Media.java
@@ -0,0 +1,15 @@
+
+ class Tarefa_Calculo_Media {
+
+    public static void main(String[] args) {
+    int nota1 = 4;
+    int nota2 = 9;
+    int nota3 = 8;
+    int nota4 = 2;
+
+    int mediaNotas = (nota1+ nota2 + nota3 +nota4)/4;
+
+    System.out.println(mediaNotas);
+
+    }
+}
